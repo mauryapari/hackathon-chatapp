f3b07adfe9385f6f749e74d0d04b78bdc9df33fb
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-09-07 03:15:18 -0400

    Create loading.tsx

---
+++ app/(core)/loading.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+
+interface ComponentProps {}
+
+// Todo - add loading animation and make this look nice
+const Component: FC<ComponentProps> = async ({}) => {
+  return (
+    <>
+      Loading...
+    </>
+  );
+};
+
+export default Component;
